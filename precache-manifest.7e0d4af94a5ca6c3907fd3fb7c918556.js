e7b37e92446e79c2be70283618e0e0c435969a52
ashishkumar <Ashish.Kumar.5@team.telstra.com>
2020-04-05 15:45:47 +0530

    Updates

---
+++ precache-manifest.7e0d4af94a5ca6c3907fd3fb7c918556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1ffb9d0492e2d0fd96f20fda2d4c2e",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "1201599be289bc6b3333",
+    "url": "/covid19-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "878f95acf3f41b3eae43",
+    "url": "/covid19-tracker/static/js/2.d46046c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-tracker/static/js/2.d46046c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1201599be289bc6b3333",
+    "url": "/covid19-tracker/static/js/main.f351ca23.chunk.js"
+  },
+  {
+    "revision": "dbae980a6809b37e52bb",
+    "url": "/covid19-tracker/static/js/runtime-main.56e53f11.js"
+  }
+]);
