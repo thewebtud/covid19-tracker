db527d33dab0850823b1c18a23aa8981e7be1f39
ashishkumar <Ashish.Kumar.5@team.telstra.com>
2020-04-12 20:53:27 +0530

    hashrouter introduced

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
 import WorldwideContainer from './containers/WorldwideContainer';
 import temp from './components/worldwideData/temp';
 import IndianStateWise from './components/indianStateWise/indianStateWise';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app_container">
-      <Router>
+      <HashRouter>
 
-        <nav role="navigation" id="nav_bar_hamburger">
+      <nav role="navigation" id="nav_bar_hamburger">
           <div id="menuToggle">
             <input type="checkbox" />
             <span></span>
@@ -19,32 +24,20 @@ function App() {
             <span></span>
 
             <ul id="menu">
-              <a href="/covid19-tracker"><li>Worldwide</li></a>
-              <a href="/india-statewise"><li>India-Statewise</li></a>
-              <a href="/coming-soon"><li>Coming Soon</li></a>
+              <li><NavLink exact to="/">Worldwide</NavLink></li>
+            <li><NavLink to="/indiastatewise">IndianStateWise</NavLink></li>
+            <li><NavLink to="/comingsoon">ComingSoon</NavLink></li>
             </ul>
           </div>
         </nav>
 
-
-
-        <div className="routingContainer">
-          {/* <nav className="NavLink ">
-            <ul className="header width">
-              <li><NavLink to={'/covid19-tracker'} className="nav-link"> Worldwide </NavLink></li>
-              <li><NavLink to={'/india-statewise'} className="nav-link">India-Statewise</NavLink></li>
-              <li><NavLink to={'/coming-soon'} className="nav-link">Coming Soon</NavLink></li>
-            </ul>
-          </nav> */}
-          <Switch>
-            {/* <Route path="/" exact component={WorldwideContainer} /> */}
-            <Route path="/covid19-tracker" component={WorldwideContainer} />
-            <Route path="/india-statewise" component={IndianStateWise} />
-            <Route path="/coming-soon" component={temp} />
-          </Switch>
-          <hr />
-        </div>
-      </Router>
+          <div className="content">
+            <Route exact path="/" component={WorldwideContainer} />
+            <Route exact path="/covid19-tracker" component={WorldwideContainer} />
+            <Route path="/indiastatewise" component={IndianStateWise} />
+            <Route path="/comingsoon" component={temp} />
+          </div>
+      </HashRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
-const app = (
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
-)
-
-ReactDOM.render(app, document.getElementById('root')
+ReactDOM.render(<App/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
